459fd7301d9fbe05a353c7d91b8746d260f8ff08
Renzo Fernandez <renzofernandezg.rf@gmail.com>
2024-03-16 02:18:16 -0400

    responsive info card for profile v2

--- src/ProfilePageV2.js
+++ src/ProfilePageV2.js
@@ -3,16 +3,19 @@ import React from 'react';
 const ProfilePageV2 = () => {
   const logoSrc = `${process.env.PUBLIC_URL}/logoh.png`;
   const bannerSrc = `${process.env.PUBLIC_URL}/banner.jpg`; // Path to the banner image
+  const profilePicSrc = `${process.env.PUBLIC_URL}/profile.jpg`; // Path to the profile image
 
   return (
     <div className="min-h-screen flex flex-col bg-white md:bg-gray-100">
       <header className="w-full h-14 md:h-20 flex items-center md:justify-start justify-center bg-white fixed top-0 z-50 shadow-md">
         <img src={logoSrc} alt="Logo" className="h-11 md:h-16 md:ml-9"/>
       </header>
-      <div className="w-full flex flex-col items-center px-0 mt-28">
-        <div className="w-full max-w-4xl bg-white shadow-lg">
-          <img src={bannerSrc} alt="Banner" className="w-full" style={{ height: '70%' }} />
-          <h2 className="text-xl font-semibold p-4">Title Text Here</h2>
+      <div className="w-full flex flex-col items-center px-0 mt-14 md:mt-28">
+        <div className="w-full max-w-6xl bg-white shadow-lg relative text-center md:text-left md:rounded-lg">
+          <img src={bannerSrc} alt="Banner" className="w-full md:rounded-t-lg" />
+          <img src={profilePicSrc} alt="Profile" className="rounded-full border-6 border-white absolute left-1/2 md:left-32 transform -translate-x-1/2 -translate-y-1/2 w-36 h-36" />
+          <h2 className="text-3xl font-bold text-red-400 mt-20 md:mt-6 md:mx-60">Alberto Garcia Perez</h2>
+          <p className="text-base text-gray-400 mx-24 my-3 italic transform md:mx-60 md:mb-10">Su sabiduria y generosidad serán recordados por siempre</p>
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}", // Adjust the glob patterns as needed for your project's file structure
   ],
   theme: {
-    extend: {},
+    extend: {
+      borderWidth: {
+        '6': '6px', // or '0.375rem', or '3px' if you want a 3px border
+      }
+    },  
   },
   plugins: [],
 };
